82e09f3d6b84bf3687c1fe3f0430b5f635dda831
Makeev Roman <makeevr032@mail.ru>
2024-07-18 19:31:08 +0300

    Initial Commit

---
+++ surf_school_final/surf_school_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  surf_school_finalApp.swift
+//  surf_school_final
+//
+//  Created by Roman on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct surf_school_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
